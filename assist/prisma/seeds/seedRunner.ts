c35d23b78ca3d2a816bfea322a91ca0d5bea8d59
David Maier <david.maier@atempo.at>
2022-04-19 09:04:05 +0200

    fix: remove unused seed municipalities

--- assist/prisma/seeds/seedRunner.ts
+++ assist/prisma/seeds/seedRunner.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-import { seedMunicipalities, seedRegions } from "./regions";
+import { seedRegions } from "./regions";
 
 export async function seedRunner(prisma: PrismaClient): Promise<void> {
   await seedRegions(prisma);
